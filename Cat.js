ea439f30d0f6ea7a85a0a24ad5ac4b5abc3dcba6
NowfxWho <nowf.study@gmail.com>
2023-02-26 17:12:25 +0700

    Add dead method for Cat

--- Cat.js
+++ Cat.js
@@ -1,8 +1,12 @@
 function meows() {
   this.stomach = [];
+  this.dead = false;
 }
 
 Cat.proptotype.eat = function (mouse) {
   this.stomach.push();
+};
+Cat.proptotype.die = function () {
+  this.dead = true;
 };
 module.exports = Cat;
